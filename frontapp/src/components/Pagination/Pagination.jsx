2150f7ed38e4fdc001c9fdf5a6d4a644bf29e090
Игорь <severuykhin.igor@gmail.com>
2018-09-09 14:01:38 +0300

    Redesigned pagination logic for a large number of pages

--- frontapp/src/components/Pagination/Pagination.jsx
+++ frontapp/src/components/Pagination/Pagination.jsx
@@ -6,7 +6,89 @@ import './Pagination.css';
 const Pagination = (props) => {
 
 	const shift = 45;
-	const pages = Math.ceil(props.total / shift);
+	const pages = props.total ? Math.ceil(props.total / shift) : 0;
+
+	/**
+	 * Build navlink component based on given params
+	 * @param {number} index 
+	 * @param {string} activeClass 
+	 * @param {string} link 
+	 */
+	const buildLink = (index, link, queryParams) => {
+
+		let isActive = link === window.location.pathname;
+		let activeClass = isActive ? 'active' : '';
+
+		return (
+			<li key={`pagination-page-${index}`}>
+				<NavLink
+					exact
+					className={activeClass}
+					to={link + queryParams}>
+					{ index }
+				</NavLink>
+			</li>
+		);
+	}
+
+	/**
+	 * Just builds empty item
+	 * @param {string} id
+	 */
+	const buildStub = (id) => {
+		return (<li key={`pagination-page-stub-${id}`}>
+				<span>...</span>
+				</li>);
+	}
+
+	/**
+	 * Build links with gap if number of pages is bigger than 11
+	 * @param { number } pages - Total number of available pages 
+	 * @param { number } current - Current active page 
+	 * @param { string } baseLink - canonical
+	 * @param { string } queryParams - all params in query string  
+	 */
+	const buildWithGap = (pages, baseLink, queryParams, current) => {
+		const links = [];
+
+		// If current page is near edge of pages
+		// Just buld last 8 pages
+		if (current + 4 >= pages - 1) {
+			
+			for (let i = pages - 1; i >= pages - 8; i--) {
+				let link = `${baseLink}/${i}`
+				links.unshift(buildLink(i, link, queryParams));
+			}
+			links.unshift(buildStub('first'));
+		}
+
+		// If current page if bigger than 6 but smaller than "near edge"
+		else if (current > 6 && (current + 3 < pages - 1)) {
+			for (let i = current - 3; i <= current + 3; i++) {
+				let link = `${baseLink}/${i}`
+				links.push(buildLink(i, link, queryParams));
+			}	
+
+			links.unshift(buildStub('first'));
+			links.push(buildStub('last'));
+			
+		// else just build pages from second - to 9
+		} else {
+
+			for (let i = 2; i <= 9; i++) {
+				let link = `${baseLink}/${i}`
+				links.push(buildLink(i, link, queryParams));
+			}	
+			links.push(buildStub('last'));
+
+		}
+
+		// Insert first an last page
+		links.unshift(buildLink(1, baseLink, queryParams));
+		links.push(buildLink(pages, `${baseLink}/${pages}`, queryParams));
+
+		return links;
+	}
 
 	/**
 	 * 
@@ -14,10 +96,15 @@ const Pagination = (props) => {
 	 * @param { string } baseLink - canonical
 	 * @param { string } queryParams - all params in query string  
 	 */
-	const buildLinks = (pages, baseLink, queryParams) => {
+	const buildLinks = (pages, baseLink, queryParams, current) => {
 
-		const links = [];
-		
+		let links = [];
+
+		// If number of pages is bigger than 10 - return links with gap 
+		// with somee other logic
+		if (pages > 11) return buildWithGap(pages, baseLink, queryParams, current); 
+
+
 		for (let i = 1; i <= pages; i++) {
 
 			let link = '';
@@ -28,21 +115,7 @@ const Pagination = (props) => {
 				link = `${baseLink}/${i}`;
 			}
 
-			let isActive = link === window.location.pathname;
-			let activeClass = isActive ? 'active' : '';
-
-			link = link + queryParams;
-
-			links.push((
-				<li key={`pagination-page-${i}`}>
-					<NavLink
-						exact
-						className={activeClass}
-						to={link}>
-						{ i }
-					</NavLink>
-				</li>
-			));
+			links.push(buildLink(i, link, queryParams));
 		}
 
 		return links;
@@ -97,7 +170,7 @@ const Pagination = (props) => {
 					{ getPrevLink(props.current, pages, props.baseLink, props.queryParams) }
 				</div>
 				<ul className="page_nav d-flex flex-row">
-					{ buildLinks(pages, props.baseLink, props.queryParams) }
+					{ buildLinks(pages, props.baseLink, props.queryParams, props.current) }
 				</ul>
 				<div className="page_next d-flex flex-column align-items-center justify-content-center">
 					{ getNextLink(props.current, pages, props.baseLink, props.queryParams) }
