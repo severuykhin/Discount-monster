27c173035ba620389f5c75756cda622a84eb149a
Игорь <severuykhin.igor@gmail.com>
2018-11-08 18:45:50 +0300

    Responsive mini app drawer

--- backapp/src/components/App/App.jsx
+++ backapp/src/components/App/App.jsx
@@ -1,63 +1,149 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PrimarySearchAppBar from '../Header/Header';
-import Menu from '../Menu/Menu';
-import Routes from '../Routes/Routes';
-import store from '../../redux/index';
-import { setStores } from '../../ducks/Stores';
-import StoresApi from '../../utils/api/StoresAPI';
-import Login from '../Pages/Login';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import IconButton from "@material-ui/core/IconButton";
+import Routes from "../Routes/Routes";
+import store from "../../redux/index";
+import { setStores } from "../../ducks/Stores";
+import StoresApi from "../../utils/api/StoresAPI";
+import Login from "../Pages/Login";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "../Menu/Menu";
 
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36
+  },
+  hide: {
+    display: "none"
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  }
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
 
   componentDidMount() {
-
     // Fetching init app data
-    console.log('app mounted');
     const storesApi = new StoresApi();
 
-    Promise.all([
-      storesApi.fetchStores()
-    ]).then( dataCollection => {
-        store.dispatch(setStores(dataCollection[0].data))
-    }).catch( (e) => {
-      console.log(e);
-    }); 
-
+    Promise.all([storesApi.fetchStores()])
+      .then(dataCollection => {
+        store.dispatch(setStores(dataCollection[0].data));
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
 
   render() {
-
     if (false === this.props.user.loggedIn) {
-      return <Login />
+      return <Login />;
     }
+
+    const { classes, theme } = this.props;
 
     return (
       <BrowserRouter>
         <div className="app">
-          <PrimarySearchAppBar />
-          <div className="main">
-              <div className="menu">
-                  <Menu />
-              </div>
-              <div className="content">
-                  <Routes />
-              </div>
+          <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+              position="fixed"
+              className={classNames(classes.appBar, {
+                [classes.appBarShift]: this.state.open
+              })}
+            >
+              <Toolbar disableGutters={!this.state.open}>
+                <IconButton
+                  color="inherit"
+                  aria-label="Open drawer"
+                  onClick={this.toggleMenu}
+                  className={classNames(classes.menuButton, {
+                    [classes.hide]: this.state.open
+                  })}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Betterdeals
+                </Typography>
+              </Toolbar>
+            </AppBar>
+
+            <Menu 
+              open={this.state.open} 
+              toggle={this.toggleMenu} />
+
+            <main className={classes.content}>
+              <div className={classes.toolbar} />
+              <Routes />
+            </main>
           </div>
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  user : state.user
+  user: state.user
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({});
 
-});
+const connectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withStyles(styles, { withTheme: true })(connectedApp);
--- backapp/src/components/Menu/Menu.jsx
+++ backapp/src/components/Menu/Menu.jsx
@@ -1,75 +1,152 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/core/styles';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import FilterList from '@material-ui/icons/FilterList';
-import ShopTwo from '@material-ui/icons/ShopTwo';
-import ViewModule from '@material-ui/icons/ViewModule';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from "@material-ui/core/styles";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+import FilterList from "@material-ui/icons/FilterList";
+import ShopTwo from "@material-ui/icons/ShopTwo";
+import ViewModule from "@material-ui/icons/ViewModule";
+import { NavLink } from "react-router-dom";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import classNames from "classnames";
+import IconButton from "@material-ui/core/IconButton";
+
+import './Menu.css';
+
+const drawerWidth = 240;
 
 const styles = theme => ({
-  menuItem: {
-    '&:focus': {
-      backgroundColor: theme.palette.primary.main,
-      '& $primary, & $icon': {
-        color: theme.palette.common.white,
-      },
-    },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
-  primary: {},
-  icon: {},
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing.unit * 9
+    }
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  }
 });
 
-function ListItemComposition(props) {
-  const { classes } = props;
+class Menu extends React.Component {
+  render() {
+    const { classes, theme, open, toggle } = this.props;
 
-  return (
-    <Paper>
-      <MenuList>
-        <NavLink to="/stores">
+    return (
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: classNames(
+            classes.drawerPaper,
+            !open && classes.drawerPaperClose
+          )
+        }}
+        open={open}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={toggle}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <NavLink to="/stores">
             <MenuItem className={classes.menuItem}>
-                    <ListItemIcon className={classes.icon}>
-                        <ShoppingCart />
-                    </ListItemIcon>
-                    <ListItemText classes={{ primary: classes.primary }} inset primary="Магазины" />
+              <ListItemIcon className={classes.icon}>
+                <ShoppingCart />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Магазины"
+              />
             </MenuItem>
-        </NavLink>
-        <NavLink to="/tags">
+          </NavLink>
+          <NavLink to="/tags">
             <MenuItem className={classes.menuItem}>
-                    <ListItemIcon className={classes.icon}>
-                        <FilterList />
-                    </ListItemIcon>
-                    <ListItemText classes={{ primary: classes.primary }} inset primary="Теги фильтрации" />
+              <ListItemIcon className={classes.icon}>
+                <FilterList />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Теги фильтрации"
+              />
             </MenuItem>
-        </NavLink>
-        <NavLink to="/categories">
+          </NavLink>
+          <NavLink to="/categories">
             <MenuItem className={classes.menuItem}>
-                    <ListItemIcon className={classes.icon}>
-                        <ShopTwo />
-                    </ListItemIcon>
-                    <ListItemText classes={{ primary: classes.primary }} inset primary="Категории" />
+              <ListItemIcon className={classes.icon}>
+                <ShopTwo />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Категории"
+              />
             </MenuItem>
-        </NavLink>
-        <NavLink to="/products">
+          </NavLink>
+          <NavLink to="/products">
             <MenuItem className={classes.menuItem}>
-                    <ListItemIcon className={classes.icon}>
-                        <ViewModule />
-                    </ListItemIcon>
-                    <ListItemText classes={{ primary: classes.primary }} inset primary="Товары" />
+              <ListItemIcon className={classes.icon}>
+                <ViewModule />
+              </ListItemIcon>
+              <ListItemText
+                classes={{ primary: classes.primary }}
+                inset
+                primary="Товары"
+              />
             </MenuItem>
-        </NavLink>
-      </MenuList>
-    </Paper>
-  );
+          </NavLink>
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    );
+  }
 }
+// <Paper>
+//   <MenuList>
 
-ListItemComposition.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+//   </MenuList>
+// </Paper>
 
-export default withStyles(styles)(ListItemComposition);
+export default withStyles(styles, { withTheme: true })(Menu);
