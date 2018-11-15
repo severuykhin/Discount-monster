a31b58351ef3b42c5767bea5747ce683a67fccb4
Игорь <severuykhin.igor@gmail.com>
2018-11-15 23:36:13 +0300

    Links form refactor

--- backapp/src/components/Link/LinkForm.jsx
+++ backapp/src/components/Link/LinkForm.jsx
@@ -5,23 +5,16 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import ReactDOM from "react-dom";
 import { main } from "../../utils/config/main";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
+import SingleSelect from '../Reusable/SingleSelect';
 
+const formattedStatues = main.statuses.map(i => ({ name: i.name, id: i.value }));
 
 const styles = theme => ({
   root: {
     display: "flex",
     justifyContent: "center",
     flexWrap: "wrap"
-  },
-  chip: {
-    margin: theme.spacing.unit
   },
   textField: {
     marginBottom: 25
@@ -57,14 +50,6 @@ class LinkForm extends Component {
     }
   }
 
-  componentDidMount() {
-    this.setState({
-      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
-      categoryLabelWidth: ReactDOM.findDOMNode(this.categoryLabelWidth).offsetWidth,
-      storeLabelWidth: ReactDOM.findDOMNode(this.storeLabelWidth).offsetWidth,
-    });
-  }
-
   handleSubmit = e => {
     e.preventDefault();
     this.props.formSubmitHandler(this.state.values);
@@ -77,18 +62,7 @@ class LinkForm extends Component {
         [e.target.name]: e.target.value
       };
 
-      console.log(values);
-
       this.setState({ values });
-  }
-
-  handleStatusChange = (e) => {
-    let status = e.target.value;
-    let values = {
-      ...this.state.values,
-      status
-    }
-    this.setState({ values })
   }
 
   render() {
@@ -130,105 +104,30 @@ class LinkForm extends Component {
             </div>
 
             <div className="form-item">
-              <FormControl 
-                variant="outlined" 
-                className={classes.formControl}>
-                <InputLabel
-                  ref={ref => {
-                    this.InputLabelRef = ref;
-                  }}
-                  htmlFor="outlined-age-simple">
-                  Статус
-                </InputLabel>
-                <Select
-                  value={this.state.values.status}
-                  onChange={this.handleStatusChange}
-                  width={200}
-                  input={
-                    <OutlinedInput
-                      name="status"
-                      labelWidth={this.state.labelWidth}
-                      id="outlined-age-simple"
-                    />
-                  }
-                >
-                  {main.statuses.map(item => {
-                    return (
-                      <MenuItem key={item.name} value={item.value}>
-                        {item.name}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
+              <SingleSelect
+                variants={this.props.stores}
+                name="store"
+                handleChange={this.handleInputChange}
+                value={this.state.values.store} 
+                title="Для магазина" />
             </div>
 
             <div className="form-item">
-              <FormControl 
-                variant="outlined" 
-                className={classes.formControl}>
-                <InputLabel
-                  ref={ref => {
-                    this.storeLabelWidth = ref;
-                  }}
-                  htmlFor="outlined-age-simple">
-                  Для магазина
-                </InputLabel>
-                <Select
-                  value={this.state.values.store}
-                  onChange={this.handleInputChange}
-                  width={200}
-                  input={
-                    <OutlinedInput
-                      name="store"
-                      labelWidth={this.state.storeLabelWidth}
-                      id="outlined-age-simple"
-                    />
-                  }
-                >
-                  {this.props.stores.map(item => {
-                    return (
-                      <MenuItem key={item.name} value={item.id}>
-                        {item.name}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
+              <SingleSelect
+                variants={this.props.categories}
+                name="category"
+                handleChange={this.handleInputChange}
+                value={this.state.values.category} 
+                title="Категория" />
             </div>
 
             <div className="form-item">
-              <FormControl 
-                variant="outlined" 
-                className={classes.formControl}>
-                <InputLabel
-                  ref={ref => {
-                    this.categoryLabelWidth = ref;
-                  }}
-                  htmlFor="outlined-age-simple">
-                  Категория
-                </InputLabel>
-                <Select
-                  value={this.state.values.category}
-                  onChange={this.handleInputChange}
-                  width={200}
-                  input={
-                    <OutlinedInput
-                      name="category"
-                      labelWidth={this.state.categoryLabelWidth}
-                      id="outlined-age-simple"
-                    />
-                  }
-                >
-                  {this.props.categories.map(item => {
-                    return (
-                      <MenuItem key={item.name} value={item.id}>
-                        {item.name}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
+              <SingleSelect
+                variants={formattedStatues}
+                name="status"
+                handleChange={this.handleInputChange}
+                value={this.state.values.status} 
+                title="Статус" />
             </div>
 
             <div className="stores__form-item">
---
+++ backapp/src/components/Reusable/SingleSelect.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import ReactDOM from "react-dom";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  textField: {
+    marginBottom: 25
+  },
+  formControl: {
+    minWidth: 200,
+    marginBottom: 15
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2
+  }
+});
+
+class SingleSelect extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labelWidth: 0
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
+    });
+  }
+
+  render() {
+
+    const { classes, title, value, name, variants, handleChange } = this.props;
+
+    return (
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel
+          ref={ref => {
+            this.InputLabelRef = ref;
+          }}
+          htmlFor="outlined-age-simple">
+
+        { title }
+
+        </InputLabel>
+        <Select
+          value={ value }
+          onChange={ handleChange }
+          width={200}
+          input={
+            <OutlinedInput
+              name={ name }
+              labelWidth={this.state.labelWidth}
+              id="outlined-age-simple"/>
+          }>
+
+          { variants.map(item => {
+            return (
+              <MenuItem key={item.name} value={item.id}>
+                {item.name}
+              </MenuItem>
+            );
+          }) }
+
+        </Select>
+      </FormControl>
+    );
+  }
+}
+
+export default withStyles(styles)(SingleSelect);
